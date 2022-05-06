0097cdff62aabba064962bb4102bcfe4618cd59e
arthur.neto@luizalabs.com <arthur.neto@luizalabs.com>
2022-05-06 00:02:33 -0300

    Criado menu-expanded

---
+++ main.js
@@ -0,0 +1,14 @@
+function onScroll() {
+  navigation.classList.add('scroll')
+  if (scrollY == 0) {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
